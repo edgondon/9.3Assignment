77e7efc83dd727a762bed1ef0aafdaac03080fa2
Daniel P Landon <dougedd@yahoo.com>
2019-08-28 01:14:09 -0700

    first upload

---
+++ index.js
@@ -0,0 +1,35 @@
+
+$(document).ready(function () {
+
+    
+    $(document).on('click', 'button', function (event) {
+      event.preventDefault();
+      let task = document.getElementById('shopping-list-entry').value;
+      document.forms['js-shopping-list-form'].reset();
+      $('.shopping-list').prepend(`<li>
+          <span class="shopping-item">`+ task + `</span>
+          <div class="shopping-item-controls">
+            <button class="shopping-item-toggle">
+              <span class="button-label">check</span>
+            </button>
+            <button class="shopping-item-delete">
+              <span class="button-label">delete</span>
+            </button>
+          </div>
+        </li>`
+        )
+        event.preventDefault();
+    });
+  
+    $("ul").on("click", ".shopping-item-toggle", function() {
+        $(this).closest("li").find(".shopping-item").toggleClass("shopping-item__checked");
+    });
+  
+    $("ul").on("click", ".shopping-item-delete", function() {
+        $(this).closest("li").remove();
+    });
+  
+  
+  
+  
+  })
